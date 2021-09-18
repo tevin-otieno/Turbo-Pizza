cdc23b7ab76d5e1fd1aa3189652b9690ce5090d1
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-09-18 18:46:06 +0300

    adding the user interface for people to communicate with app

--- script.js
+++ script.js
@@ -8,20 +8,17 @@ class Pizza {
     };
 };
 
-let pPizza = new Pizza('1000', '1.1', '1.2','1.3','1.4','1.5')
-console.log(pPizza)
-console.log(pPizza.topping)
-console.log(typeof(pPizza.cost))
-console.log(pPizza.price) //1320
+//let pPizza = new Pizza('1000', '1.1', '1.2','1.3','1.4','1.5')
+//console.log(pPizza)
+//console.log(pPizza.topping)
+//console.log(typeof(pPizza.cost))
+//console.log(pPizza.price) //1320
 
-//$(document).ready(function() {
-//    $("#pizza-form").submit(function(event) {
-//      event.preventDefault();
-//      var size = $("select#size").val();
-//      var toppings = $("#toppings").val();
-//      var myPizzaOrder = new Pizzaorder(size, toppings);
-//      var cost = myPizzaOrder.cost();
-//      $("#price").text("$" + cost);
-//      $(".show-pizza").show();
-//    });
-//});
+$(document).ready(function() {
+    $("form#pizza-form").submit(function(event) {
+      event.preventDefault();
+    
+      
+      console.log('set');
+    });
+});
