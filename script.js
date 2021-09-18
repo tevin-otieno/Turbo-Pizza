944be926fbf8a8b2f7576ae0e1132be34e084f9e
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-09-18 17:41:02 +0300

    the pizza price constractor works like a charm

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 class Pizza {
-    constructor (size, crust, topping){
+    constructor (size, crust, ...topping){
         this.size = size;
         this.crust = crust;
-        this.topping = topping;
+        this.topping = topping.reduce((previous, current) => {return previous * current;});
+        this.cost = size * crust * this.topping
+        this.price = this.cost.toFixed(2);
     };
 };
 
-let pPizza = new Pizza('Medium', 'stuffed', 'pepperoni')
+let pPizza = new Pizza('1000', '1.1', '1.2','1.3','1.4','1.5')
 console.log(pPizza)
+console.log(pPizza.topping)
+console.log(typeof(pPizza.cost))
+console.log(pPizza.price) //1320
 
-$(document).ready(function() {
-    $("#pizza-form").submit(function(event) {
-      event.preventDefault();
-      var size = $("select#size").val();
-      var toppings = $("#toppings").val();
-      var myPizzaOrder = new Pizzaorder(size, toppings);
-      var cost = myPizzaOrder.cost();
-      $("#price").text("$" + cost);
-      $(".show-pizza").show();
-    });
-  });
+//$(document).ready(function() {
+//    $("#pizza-form").submit(function(event) {
+//      event.preventDefault();
+//      var size = $("select#size").val();
+//      var toppings = $("#toppings").val();
+//      var myPizzaOrder = new Pizzaorder(size, toppings);
+//      var cost = myPizzaOrder.cost();
+//      $("#price").text("$" + cost);
+//      $(".show-pizza").show();
+//    });
+//});
